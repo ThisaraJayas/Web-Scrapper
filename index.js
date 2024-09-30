98400ee90d9ae01c37b099d387bcc5d438d25a4e
Thisara <sasmithajayasinghe1@gmail.com>
2024-09-30 22:08:11 +0530

    folder struc

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import puppeteer from 'puppeteer';
 
 const app = express();
+
+app.get('/hello', async(req,res)=>{
+    res.send({hello:"hello word"});
+})
 
 app.listen(3000, ()=>{
     console.log("Server Running at port 3000");
