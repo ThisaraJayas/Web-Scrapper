78e90b89109674991b50a71e06c2302d347be5bf
Thisara <sasmithajayasinghe1@gmail.com>
2024-10-01 13:39:21 +0530

    all complete

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import express from 'express';
-import puppeteer from 'puppeteer';
 import ScraperRoute from './routes/scraperRoute.js'
 
 const app = express();
-
+app.use(express.json())
 
 app.use('/search',ScraperRoute )
 
 
 app.listen(3000, () => {
-    console.log('Server Running at port 3000');
+    console.log('Server running at port 3000');
 });
--- utils/puppeterUtil.js
+++ utils/puppeterUtil.js
@@ -1,50 +1,72 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 
 export const scraping = async (keyword) => {
-    try {
-        const browser = await puppeteer.launch({ headless: true });
-        const page = await browser.newPage();
+  try {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
 
-        const searchURL = `https://ikman.lk/en/ads?query=${keyword}`;
-        await page.goto(searchURL, { waitUntil: 'networkidle2', timeout: 200000 });
+    let currentPage = 1;
+    const searchURL = `https://ikman.lk/en/ads?query=${keyword}&page=${currentPage}`;
+    await page.goto(searchURL, { waitUntil: "networkidle2", timeout: 400000 });
 
-        const cardListings = await page.$$eval('li.gtm-normal-ad a.card-link--3ssYv.gtm-ad-item', links => {
-            return links.map(link => ({
-                title: link.getAttribute('title'),
-                url: link.href,
-            }));
+    const detailedListing = [];
+
+    const scrapeCurrentPage = async () => {
+      const cardListings = await page.$$eval("li.gtm-normal-ad a.card-link--3ssYv.gtm-ad-item",(links) => {
+          return links.map((link) => ({
+            title: link.querySelector("h2").innerText,
+            price: link.querySelector(".price--3SnqI span").innerText,
+            url: link.href,
+          }));
+        }
+      );
+
+      for (const listing of cardListings) {
+        const itemPage = await browser.newPage();
+        await itemPage.goto(listing.url, {
+          waitUntil: "networkidle2",
+          timeout: 400000,
         });
 
+        const postedDate = await itemPage.$eval(".sub-title--37mkY",(element) => {
+            const text = element.innerText;
+            const dateMatch = text.match(/Posted on (.+?),/);
+            return dateMatch[0];
+          }
+        );
 
-        const detailedListing = [];
+        const location = await itemPage.$eval(".subtitle-wrapper--1M5Mv",(element) => {
+            const city = element.querySelector(".subtitle-location-link--1q5zA span").innerText;
+            return city;
+          }
+        );
 
-        for (const listing of cardListings) {
-            const itemPage = await browser.newPage();
-            await itemPage.goto(listing.url, { waitUntil: 'networkidle2', timeout: 200000 });
+        detailedListing.push({
+          ...listing,
+          postedDate,
+          location,
+        });
 
-            const postedDate = await itemPage.$eval('.sub-title--37mkY', element => {
-                const text = element.innerText;
-                const dateMatch = text.match(/Posted on (.+?),/);
-                return dateMatch[0];
-            });
+        await itemPage.close();
+      }
+    };
 
-            const location = await itemPage.$eval('.subtitle-wrapper--1M5Mv', element => {
-                const city = element.querySelector('.subtitle-location-link--1q5zA span').innerText.trim();
-                return city; 
-            });
+    while (true) {
+      await scrapeCurrentPage();
 
-            detailedListing.push({
-                ...listing,
-                postedDate,
-                location
-            });
+      const nextPageTag = `li.gtm-go-to-page[data-id="${currentPage + 1}"]`;
+      const nextPageExists = await page.$(nextPageTag);
+      if (!nextPageExists) {
+        break;
+      }
 
-            await itemPage.close();
-        }
-
-        await browser.close(); 
-        return detailedListing
-    } catch (error) {
-        console.log("pupeter util error",error);
+      currentPage++;
     }
+
+    await browser.close();
+    return detailedListing;
+
+  } catch (error) {
+    console.log("pupeter util error", error);
+  }
 };
