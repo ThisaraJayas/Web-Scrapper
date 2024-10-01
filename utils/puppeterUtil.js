b6789b751446f9d5053c256b469b6a4dfcbb0b31
Thisara <sasmithajayasinghe1@gmail.com>
2024-10-01 09:55:13 +0530

    scraping complete

--- utils/puppeterUtil.js
+++ utils/puppeterUtil.js
@@ -1,27 +1,50 @@
 import puppeteer from 'puppeteer';
 
-export const scraping = async(keyword)=>{
+export const scraping = async (keyword) => {
     try {
         const browser = await puppeteer.launch({ headless: true });
         const page = await browser.newPage();
 
         const searchURL = `https://ikman.lk/en/ads?query=${keyword}`;
-        await page.goto(searchURL, { waitUntil: 'networkidle2', timeout: 120000 });
+        await page.goto(searchURL, { waitUntil: 'networkidle2', timeout: 200000 });
 
-        const listings = await page.$$eval('li.gtm-normal-ad a.card-link--3ssYv.gtm-ad-item', links => {
+        const cardListings = await page.$$eval('li.gtm-normal-ad a.card-link--3ssYv.gtm-ad-item', links => {
             return links.map(link => ({
-                title: link.getAttribute('title'), 
-                url: link.href, 
-                price: link.querySelector('.price--3SnqI span'),
-
+                title: link.getAttribute('title'),
+                url: link.href,
             }));
         });
 
-        await browser.close(); 
 
-        return listings; 
+        const detailedListing = [];
+
+        for (const listing of cardListings) {
+            const itemPage = await browser.newPage();
+            await itemPage.goto(listing.url, { waitUntil: 'networkidle2', timeout: 200000 });
+
+            const postedDate = await itemPage.$eval('.sub-title--37mkY', element => {
+                const text = element.innerText;
+                const dateMatch = text.match(/Posted on (.+?),/);
+                return dateMatch[0];
+            });
+
+            const location = await itemPage.$eval('.subtitle-wrapper--1M5Mv', element => {
+                const city = element.querySelector('.subtitle-location-link--1q5zA span').innerText.trim();
+                return city; 
+            });
+
+            detailedListing.push({
+                ...listing,
+                postedDate,
+                location
+            });
+
+            await itemPage.close();
+        }
+
+        await browser.close(); 
+        return detailedListing
     } catch (error) {
-        console.log("pupeter util error: ",error);
-        
+        console.error("pupeter util error",error);
     }
-}
+};
